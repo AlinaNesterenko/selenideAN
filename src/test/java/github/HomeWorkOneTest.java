0189d2c04b0e859f2028be243cb284f56eb1acfc
Alina Nesterenko <alina.zaitsava@minsk.leveris.com>
2023-09-20 14:31:50 +0300

    edit after review

--- src/test/java/github/HomeWorkOneTest.java
+++ src/test/java/github/HomeWorkOneTest.java
@@ -2,10 +2,9 @@ package github;
 
 import org.junit.jupiter.api.Test;
 
-import com.codeborne.selenide.Condition;
-
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
 import static com.codeborne.selenide.Selenide.open;
@@ -18,15 +17,12 @@ public class HomeWorkOneTest {
     // перейти в вики
     $("#wiki-tab").click();
     // убедиться что есть SA
-    $$(". js-wiki-sidebar-toggle-display").findBy(Condition.exactText("SoftAssertions"));
-    // перейти к SA
     $("[placeholder='Find a page…']").click();
     $("[id='wiki-pages-filter']").setValue("Soft");
-    $$("ul.m-0 li").findBy(text("SoftAssertions")).click();
-    $$("[class='Link--primary']").findBy(text("3. Using JUnit5 extend test class:")).shouldBe(visible);
+    $("#wiki-pages-box").shouldHave(text("SoftAssertions"));
+    // перейти к SA
+    $("#wiki-pages-box").$(byText("SoftAssertions")).click();
 
-    //перейти на страницу
-    $$("ul.mx-4 li").get(8).$("a").click();
     // проверка заголовка
     $$("h4").findBy(text("3. Using JUnit5 extend test class:")).shouldBe(visible);
     // проверка кода (скопирован по иконке)
